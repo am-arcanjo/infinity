94fcc54da5d53e9137f78a6f910187a5710b684c
Michael XG <michaelxg@gmail.com>
2024-05-22 19:33:38 -0300

    inicio projeto pokemoin pokedex

---
+++ Javascript/AULA_15/script.js
@@ -0,0 +1,11 @@
+//Lembrete: se for uma classe usamos o '.' na frente e se for um id usamos '#'
+//Por convenção, para indicar valores fixos, colocamos letra maiúscula
+
+const BASE_API_URL = "https://pokeapi.co/api/v2"
+
+
+const containerPokemons = document.querySelector(".pokemons");
+
+function captarPokemons() {
+    
+}
