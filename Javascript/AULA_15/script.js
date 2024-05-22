9cd677c1db5f05c5a5da6158f5858c099a186d28
Michael XG <michaelxg@gmail.com>
2024-05-22 20:04:43 -0300

    ajustes script e style

--- Javascript/AULA_15/script.js
+++ Javascript/AULA_15/script.js
@@ -5,7 +5,25 @@ const BASE_API_URL = "https://pokeapi.co/api/v2"
 
 
 const containerPokemons = document.querySelector(".pokemons");
+let pokemons = [];
 
 function captarPokemons() {
-    
+    fetch(`${BASE_API_URL}/pokemon?limit=40`)
+    .then((resposta) => resposta.json())
+    .then(dado => {
+        pokemons = dado.results
+
+        captarPokemon()
+    })
+}
+
+function captarPokemon() {
+    for (let pokemon of pokemons) {
+        fetch(pokemon.url)
+        .then(resposta => resposta.json())
+        .then(poke => {
+            containerPokemons.innerHTML += 
+            `<img src="${poke.sprites.versions["genaration-v"]["black-white"].animated.front_default}"/>`
+        })
+    }
 }
