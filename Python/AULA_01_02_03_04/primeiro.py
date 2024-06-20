bbfe2cd4b6cc6a48cdb428afff8212d11db88d08
Michael XG <michaelxg@gmail.com>
2024-06-19 21:38:27 -0300

    primeiras funções

--- Python/AULA_01_02_03_04/primeiro.py
+++ Python/AULA_01_02_03_04/primeiro.py
@@ -1,10 +1,24 @@
+# Se eu selecionar uma trecho de código e apertar ctrl + : , ele comenta o trecho >> Alías estou comentando só pra não ficarem coisas demais impressas
 mensagem = "Hello World"
 dia_aniversario = 11
 mes_aniversario = 9
 estou_matriculado = True
 segunda_mensagem = "It's me"
 
-print(mensagem)
-print(dia_aniversario)
-print(mes_aniversario)
-print(mensagem + ", " +  segunda_mensagem)
+# Print >> função de imprimir
+# Type >> função de buscar tipo / Observe que todas as vezes que um valor é inputado, este valor terá tipo string; também vale mesmo que não seja guardado o valor em uma variável / Para fazer a 'conversão', é necessário declarar o tipo preemptivamente, note a linha 21
+# Input >> função que prompta o recebimento de um valor
+
+# print(mensagem)
+# print(dia_aniversario)
+# print(mes_aniversario)
+# print(mensagem + ", " +  segunda_mensagem)
+# print(type(estou_matriculado))
+
+# nome = input("Digite seu nome: ")
+# print(nome)
+# print(type(input("Número:")))
+# numero = int(input("Digite algum número qualquer: "))
+# print(type(numero))
+
+
