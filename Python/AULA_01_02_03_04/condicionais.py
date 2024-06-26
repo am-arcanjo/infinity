d51cac4da7163df1aa81aae26b5f0a42fd73fce0
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-06-26 20:31:14 -0300

    aula 2 condicionais python

---
+++ Python/AULA_01_02_03_04/condicionais.py
@@ -0,0 +1,8 @@
+##Exercícios de fixação
+
+#numero1 = input('Por gentileza, digite um número: ')
+#numero2 = input('Agora, digite mais um número para somar ao anterior: ')
+
+#print(int(numero1) + int(numero2))
+
+
