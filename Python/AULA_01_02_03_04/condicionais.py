0e33b46dcbce70a77c40461a34dcf4526940d9eb
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-06-26 20:55:08 -0300

    exercícios de fixação dos segmentos

--- Python/AULA_01_02_03_04/condicionais.py
+++ Python/AULA_01_02_03_04/condicionais.py
@@ -1,4 +1,5 @@
-##Exercícios de fixação
+#PARTE 1 - ARITMÉTICOS
+#Peça ao usuário para digitar dois números inteiros e imprima a soma deles.
 
 #numero1 = input('Por gentileza, digite um número: ')
 #numero2 = input('Agora, digite mais um número para somar ao anterior: ')
@@ -6,3 +7,47 @@
 #print(int(numero1) + int(numero2))
 
 
+
+#PARTE 2 - COMPARAÇÃO
+#Solicite dois números do usuário e exiba True se eles forem diferentes, ou False se forem iguais.
+
+# numero1 = input('Por gentileza, digite um número: ')
+# numero2 = input('Agora, digite mais um número para compararmos ao anterior: ')
+
+# if (numero1 != numero2):
+#     print('Seus números são diferentes; diferença = ' + str(True))
+# else:
+#     print('Seus números são iguais; diferença =  ' + str(False))
+
+
+
+#PARTE 3 - LÓGICOS
+#Peça ao usuário para digitar sua idade e verificar se ela é menor que 18 anos ou maior que 65 anos.
+
+# idade = input('Por gentileza, digite sua idade: ')
+
+# if (int(idade) < 18 or int(idade) > 65):
+#     print('Você tem menos de 18 ou mais de 65 anos!')
+# else:
+#     print('Sua idade está entre 18 e 65 anos')
+
+
+
+#PARTE 4 - IN E NOT IN
+
+# Escreva um programa que verifica o número do mês digitado pelo usuário e imprime a estação do ano correspondente.
+
+#Outono: de março a maio, Inverno: de Junho a Agosto, Primavera: de setembro a novembro e Verão: de dezembro a fevereiro
+
+# mes = input('Por favor, digite um mês qualquer: ')
+
+# if (mes == 'Dezembro' or mes == 'Janeiro' or mes == 'Fevereiro'):
+#     print('Este mês pertence ao Verão!')
+# elif(mes == 'Março' or mes == 'Abril' or mes == 'Maio'):
+#     print('Este mês pertence ao Outono!')
+# elif(mes == 'Junho' or mes == 'Julho' or mes == 'Agosto'):
+#     print('Este mês pertence ao Inverno!')   
+# elif(mes == 'Setembro' or mes == 'Outubro' or mes == 'Novembro'):
+#     print('Este mês pertence à Primavera!')
+
+#Eu acidentalmente esqueci de olhar o In e not in
