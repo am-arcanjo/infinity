1c8a5ae1746e83a68cba9118e8b5a54668565b09
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-06-26 21:20:12 -0300

    exercício correlacionado e adição ternário

--- Python/AULA_01_02_03_04/condicionais_problemas.py
+++ Python/AULA_01_02_03_04/condicionais_problemas.py
@@ -1,8 +1,28 @@
-#Faça um programa que peça ao usuário o tempo em anos do carro dele. Se o carro tiver mais de 3 anos, imprima informando que é um carro velho, se não, imprima que é novo.
+#PROBLEMA 1
+# Faça um programa que peça ao usuário o tempo em anos do carro dele. Se o carro tiver mais de 3 anos, imprima informando que é um carro velho, se não, imprima que é novo.
 
-idade_carro = int(input('Quantos anos tem seu carro? '))
+# idade_carro = int(input('Quantos anos tem seu carro? '))
 
-if (idade_carro > 3):
-    print('Seu carro é velho.')
-else:
-    print('Seu carro é novo.')
+# if (idade_carro > 3):
+#     print('Seu carro é velho.')
+# else:
+#     print('Seu carro é novo.')
+
+# Existe a forma ternária de fazer o mesmo que acima, que seria: 
+# idade_carro = int(input('Quantos anos tem seu carro? '))
+
+# resultado = 'Seu carro é velho.' if idade_carro > 3 else 'Seu carro é novo.'
+
+# print(resultado)
+
+#EXERCÍCIO RELACIONADO
+# Escreva um programa que verifica se o a idade digitada pelo usuário indica que ele é obrigado a votar (maior ou igual a 18 anos) ou não, e imprima uma mensagem correspondente.
+
+idade_usuario = int(input('Quantos anos você tem? '))
+
+obrigatoriedade = 'Você é obrigado a votar!' if idade_usuario >= 18 else 'Você não é obrigado a votar.'
+
+print(obrigatoriedade)
+
+#PROBLEMA 2
+# 
