4d36ea51ed552b6d3f05f1aec0db76c54c6861a3
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-07-31 20:10:51 -0300

    tuplas e listas

---
+++ Python/AULAS_PYTHON_MESMO/tuplas_e_listas.py
@@ -0,0 +1,23 @@
+#Exercícios fixação
+# 1 - Crie uma lista com os números 1 a 5 e imprima-a
+# 
+# listaNumeros = [ 1, 2, 3, 4, 5 ]
+# print(listaNumeros)
+
+# 2 - Crie uma tupla com o nome de três cidades e imrpima-a
+#
+# nomesCidades = ("Tokyo", "Joannesburgo", "São João Del Rey")
+# print(nomesCidades)
+
+# 3 - Adicione o número 6 ao final da criada no Exercício 1 e imprima-a
+#
+# listaNumeros.append(6)
+# print(listaNumeros)
+
+# 4 - Tente alterar o segundo elemento da tupla criada no Exercício 2 para "Rio de Janeiro". O que acontece?
+# Não é possível alterar pois tuplas são imutáveis.
+
+# 5 - Remova o terceiro elemento da lista criada no Exercício 1 e imprima a lista.
+#
+# listaNumeros.pop(2)
+# print(listaNumeros)
