cca2407405baddde83f89e27d505210998606585
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-08-14 19:40:31 -0300

    revisão de lõgica de programação e exercícios de fixação

---
+++ Python/AULAS_PYTHON_MESMO/revisao_logica_programacao.py
@@ -0,0 +1,10 @@
+# WHILE e FOR > Estruturas de repetição
+# FOR > for contador in range(beginning, end):
+
+
+# Exercício de fixação: escreva um progama que imprima a tabuada do número 7 de 1 a 10
+
+iterator = 0
+for iterator in range(0, 10):
+    print((iterator + 1) * 7)
+
