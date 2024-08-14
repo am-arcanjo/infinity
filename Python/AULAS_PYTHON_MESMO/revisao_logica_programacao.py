2378a19a659cfc69d4fd8b09616697710dae2db6
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-08-14 20:11:49 -0300

    mais exercícios de fixação

--- Python/AULAS_PYTHON_MESMO/revisao_logica_programacao.py
+++ Python/AULAS_PYTHON_MESMO/revisao_logica_programacao.py
@@ -4,7 +4,32 @@
 
 # Exercício de fixação: escreva um progama que imprima a tabuada do número 7 de 1 a 10
 
-iterator = 0
-for iterator in range(0, 10):
-    print((iterator + 1) * 7)
+# iterator = 0
+# for iterator in range(0, 10):
+#     print((iterator + 1) * 7)
 
+
+
+# Mais exercícios de fixação: Faça um algoritmo que leia um número e mostre todos os números inteiros de 1 a n (número inserido)
+
+# number = int(input("Digite um número para ver a contagem até ele!: "))
+# init = 0
+# for init in range(init, number):
+#     print(init + 1)
+
+
+
+# Escreva um programa que receba uma string e imprima cada caractere em uma linha separada.
+
+# word = list(input("Escreva uma palavra para ver suas letras separadamente: "))
+# n = 0
+
+# for n in range(0, len(word)):
+#     print(word[n])
+
+
+
+# Escreva um programa que conte quantas vogais estão presentes em uma String fornecida por um usuário
+
+phrase = input("Digite uma palavra ou frase e contaremos as vogais: ")
+print(phrase.count("a"))
