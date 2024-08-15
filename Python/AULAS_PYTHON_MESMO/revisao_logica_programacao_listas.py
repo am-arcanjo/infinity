d5f0fe4c3534b474fa4af4746586b347383d8764
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-08-14 21:39:17 -0300

    início desafio tesouro

--- Python/AULAS_PYTHON_MESMO/revisao_logica_programacao_listas.py
+++ Python/AULAS_PYTHON_MESMO/revisao_logica_programacao_listas.py
@@ -4,8 +4,24 @@
 
 
 # Exemplo qualquer:
-lista_afazeres = ['acordar', 'existir', 'não desistir']
+# lista_afazeres = ['acordar', 'existir', 'não desistir']
 
-for afazer in lista_afazeres:
-    print(afazer)
+# for afazer in lista_afazeres:
+#     print(afazer)
+
+
+#DESAFIO: Vocês devem criar um jogo onde o jogador precisa encontrar o "tesouro" escondido em um mapa representado por uma lista de listas (matriz). O jogador pode escolher uma posição para cavar e descobrir se encontrou o tesouro.
+
+import random
+
+treasureList = [[],[],[]]
+
+random_column = random(range(1,4)) 
+random_line = random(range(1,4))
+
+guess_column = int(input("Digite a coluna na qual você acha que o tesouro está: "))
+guess_line = int(input("Digite a linha na qual você acha que o tesouro está: "))
+
+while guess_column != random_column and guess_line != guess_line:
+    
 
