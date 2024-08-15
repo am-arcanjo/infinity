5a6cbdf9b3d4d53d97a906758905a560a810ed2a
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-08-14 21:04:56 -0300

    lista exemplo e mais anotações

--- Python/AULAS_PYTHON_MESMO/revisao_logica_programacao_listas.py
+++ Python/AULAS_PYTHON_MESMO/revisao_logica_programacao_listas.py
@@ -1,5 +1,11 @@
 # LISTAS > conjunto de variáveis, valores, etc
-# 
+# Usa-se [] para 'introduzir' uma
+# Temos métodos úteis tais como .append() para adicionar um elemento ao final da lista
 
 
-#
+# Exemplo qualquer:
+lista_afazeres = ['acordar', 'existir', 'não desistir']
+
+for afazer in lista_afazeres:
+    print(afazer)
+
