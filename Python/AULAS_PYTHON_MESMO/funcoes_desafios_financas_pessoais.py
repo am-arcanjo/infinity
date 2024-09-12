e811fe6676cfe16082c32767de424ddf2d972100
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-09-11 21:38:35 -0300

    ajustes jogo das pistas e projeto financas, falta finalizar ambos

--- Python/AULAS_PYTHON_MESMO/funcoes_desafios_financas_pessoais.py
+++ Python/AULAS_PYTHON_MESMO/funcoes_desafios_financas_pessoais.py
@@ -5,116 +5,138 @@
 # Orçamentos > utilizar um dicionário para armazenar orçamentos, onde a chave é a categoria e o valor é o valor do orçamento 
 # Sair
 
-# PAUSEI PRA REFORMULAR O JOGO DE PISTAS
+# PAUSEI PRA REFORMULAR O JOGO DE PISTAS / ONGOING
  
 
+import re
+
+
 isContinue = True
+categorias_de_gastos = {}
+receitas = {}
+despesas = {}
+orcamentos = {}
 
 def continuar_selecao(): 
-    selecao = int(input("\n Escolha o que quer fazer: \n 1. Adicionar Cliente \n 2. Remover Cliente \n 3. Buscar Cliente \n 4. Listar Clientes \n 5. Registrar Compra \n 6. Ver Histórico de Compras \n 7. Mostrar Cliente Com Mais Compras \n 8. Mostrar Total de Clientes \n 9. Sair \n "))
+    selecao = int(input("\n Escolha o que quer fazer: \n 1. Adicionar Categoria de Gasto \n 2. Remover Categoria de Gasto \n 3. Listar Categorias de Gasto \n 4. Adicionar Receita \n 5. Registrar Compra \n 6. Ver Histórico de Compras \n 7. Mostrar Cliente Com Mais Compras \n 8. Mostrar Total de Clientes \n 9. Sair \n "))
     print(selecao)
     return selecao
 
-def adicionar_categoria(nome: str, email: str):
-    cliente = {"Nome": nome, "E-mail": email, "Compras": []}
-    clientes.append(cliente)
+def adicionar_categoria(categorias: str, categoria: str):
+    categoria = {"Categoria de Gasto": categorias, "Categoria": categoria}
+    categorias_de_gastos.append(categoria)
 
-def remover_categoria(nome: str):
+def remover_categoria(nome_categoria: str):
     ocorrencias = 0
-    for cliente in clientes:
-        if(cliente["Nome"] == nome):
-            clientes.remove(cliente)
+    for categoria in categorias_de_gastos:
+        if(categoria["Nome"] == nome_categoria):
+            categorias_de_gastos.remove(categoria)
             ocorrencias += 1
     if ocorrencias == 0:
-        print("O cliente não existe, então não é possível remover o mesmo.")
+        print("Esta categoria não existe, então não é possível remover a mesma.")
     
-
-def buscar_cliente(nome: str):
-    ocorrencias = 0
-    for cliente in clientes:
-        if(cliente["Nome"] == nome):
-            print(cliente)
-            ocorrencias += 1
-    if ocorrencias == 0:
-        print("Não existe cliente com o nome inserido.")
-
 def listar_categorias():
-    for cliente in clientes:
-        print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
-     
+    for categoria in categorias_de_gastos:
+        print("Nome da Categoria de Gastos: " + categoria["Categoria de Gasto"] + "\n" + "Categoria: " + categoria["Categoria"] + "\n")
 
-def registrar_compras(nome: str):
-    ocorrencias = 0
-    for cliente in clientes:
-        if cliente["Nome"] == nome:
-            compra = input("Qual foi o produto adquirido pelo cliente? ")
-            cliente["Compras"].append(compra)
+def adicionar_receita(descricao: str, valor: int):  
+    receita = {"Descrição": descricao, "Valor": valor}
+    receitas.append(receita)
+
+def adicionar_despesa(descricao: str, valor: int, nome_da_categoria: str):
+    despesa = {"Descrição": descricao, "Valor": valor, "Categoria a qual pertence": nome_da_categoria}
+    ocorrencias = 0    
+    for categoria in categorias_de_gastos:
+        if categoria["Categoria de Gastos"] == nome_da_categoria:
+            despesas.append(despesa)
             ocorrencias += 1
     if ocorrencias == 0:
-        print("Não existe cliente com o nome inserido.")
+        print("A categoria para despesa apontada não existe.")        
+    
+def listar_receitas():       
+    for receita in receitas:
+        print("Descrição: " + receita["Descrição"] + "\n" + "Valor: " + receita["Valor"] + "\n") 
 
-def ver_historico_compras(nome: str):
+def listar_despesas():
+    for despesa in despesas:
+        print("Descrição: " + despesa["Descrição"] + "\n" + "Valor: " + despesa["Valor"] + "\n" "Categoria de gastos: " + despesa["Categoria a qual pertence"] + "\n") 
+
+def listar_despesas_categoria(categoria: str):
     ocorrencias = 0
-    for cliente in clientes:
-        if cliente["Nome"] == nome and len(cliente["Compras"]) != 0:
-            print(cliente["Compras"])
-            ocorrencias += 1
-        elif cliente["Nome"] == nome and len(cliente["Compras"]) == 0:
-            print("Este cliente não comprou nada ainda.")
+    for despesa in despesas:
+        if despesa["Categoria a qual pertence"] == categoria:
+            print("Descrição: " + despesa["Descrição"] + "\n" + "Valor: " + despesa["Valor"] + "\n" "Categoria de gastos: " + despesa["Categoria a qual pertence"] + "\n")
             ocorrencias += 1
     if ocorrencias == 0:
-        print("Não existe cliente com o nome inserido.")        
+        print("Ou a categoria apontada não existe, ou não existem despesas nesta categoria")
 
-def cliente_com_mais_compras(nome = "", email = "", compras = []):
-    cliente_mais_compras = {"Nome": nome, "E-mail": email,"Compras": compras}
-    for cliente in clientes:
-        if len(cliente["Compras"]) > len(cliente_mais_compras["Compras"]):
-            cliente_mais_compras["Compras"] = cliente["Compras"]
-            cliente_mais_compras["Nome"] = cliente["Nome"]
-            cliente_mais_compras["E-mail"] = cliente["E-mail"]
-    print("Nome do cliente: " + cliente_mais_compras["Nome"] + "\n" + "E-mail do cliente: " + cliente_mais_compras["E-mail"])        
+#PAREI AQUI DE AJUSTAR
 
-def total_clientes():
-    print(len(clientes))
+# def calcular_total_receitas():
+#     for cliente in clientes:
+#         print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
 
-def sair():
-    global isContinue
-    isContinue = False
-    print("Você saiu.")
-    return isContinue
+# def calcular_total_despesas():
+#     for cliente in clientes():
+#         print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
 
-while isContinue:
-    selecao = continuar_selecao()
+# def calcular_saldo_total():
+#     for cliente in clientes:
+#         print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
 
-    if selecao == 1:
-        nome_do_cliente = input("Indique o nome do cliente: ")
-        email_do_cliente = input("E o e-mail do cliente: ")
-        adicionar_cliente(nome_do_cliente, email_do_cliente)
+# def adicionar_orcamento():
+#     cliente = {"Nome": nome, "E-mail": email, "Compras": []}
+#     clientes.append(cliente)
 
-    elif selecao == 2:
-        nome_do_cliente = input("Indique o nome do cliente que deseja remover: ")
-        remover_cliente(nome_do_cliente)
+# def listar_orcamentos():
+#     for cliente in clientes:
+#         print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
 
-    elif selecao == 3:
-        nome_do_cliente = input("Indique o nome do cliente cujas informações deseja buscar: ")
-        buscar_cliente()
+# def verificar_despesa_por_orcamento():
+#     for cliente in clientes:
+#         print("Nome do cliente: " + cliente["Nome"] + "\n" + "E-mail do cliente: " + cliente["E-mail"] + "\n")
 
-    elif selecao == 4:
-        listar_clientes()
+# def limpar_financas():
+#     global isContinue
+#     isContinue = False
+#     despesas = {}
+#     receitas = {}
+#     orcamentos = {}
+#     print("Você limpou todo o histórico de finanças.")
+#     return isContinue
 
-    elif selecao == 5:
-        nome_do_cliente = input("Indique o nome do cliente cuja compra deseja cadastrar: ")
-        registrar_compras(nome_do_cliente)
+# while isContinue:
+#     selecao = continuar_selecao()
 
-    elif selecao == 6:
-        nome_do_cliente = input("Indique o nome do cliente cujo histórico de compras queira ver: ")
-        ver_historico_compras(nome_do_cliente)
+#     if selecao == 1:
+#         nome_do_cliente = input("Indique o nome do cliente: ")
+#         email_do_cliente = input("E o e-mail do cliente: ")
+#         adicionar_cliente(nome_do_cliente, email_do_cliente)
 
-    elif selecao == 7:
-        cliente_com_mais_compras()
+#     elif selecao == 2:
+#         nome_do_cliente = input("Indique o nome do cliente que deseja remover: ")
+#         remover_cliente(nome_do_cliente)
 
-    elif selecao == 8:
-        total_clientes()
+#     elif selecao == 3:
+#         nome_do_cliente = input("Indique o nome do cliente cujas informações deseja buscar: ")
+#         buscar_cliente()
 
-    elif selecao == 9:
-        sair()
+#     elif selecao == 4:
+#         listar_clientes()
+
+#     elif selecao == 5:
+#         nome_do_cliente = input("Indique o nome do cliente cuja compra deseja cadastrar: ")
+#         registrar_compras(nome_do_cliente)
+
+#     elif selecao == 6:
+#         nome_do_cliente = input("Indique o nome do cliente cujo histórico de compras queira ver: ")
+#         ver_historico_compras(nome_do_cliente)
+
+#     elif selecao == 7:
+#         cliente_com_mais_compras()
+
+#     elif selecao == 8:
+#         total_clientes()
+
+#     elif selecao == 9:
+#         sair()
--- Python/AULAS_PYTHON_MESMO/jogo_das_pistas.py
+++ Python/AULAS_PYTHON_MESMO/jogo_das_pistas.py
@@ -2,78 +2,199 @@
 # O professor reformulou tal que a ideia é competir com um segundo jogador.
 # O jogador 1 receberá a primeira dica e tentará acertar. Caso não acerte, o jogo irá para o jogador 2 que tentará acertar após receber a segunda dica (e saber a primeira). Após isso, caso ainda não acerte, o jogador 1 receberá a terceira dica e tentará acertar. Se não der certo, o joador 2 tenta com todas as três dicas. Se nem assim for, ambos perdem.
 
+# Fazer outra versão em que o "momento" de acerto dá pontos diferentes, ou seja, se o jogador acertar na primeira dica, ele ganhará mais pontos do que no caso dele acertar somente na segunda ou na terceira dica. E cada round consiste com um jogador iniciando. Ou seja, se no primeiro round, o jogador 1 iniciar, o segundo round quem inicia é o jogador 2. Ao final dos 10 rounds, ganha quem tem mais pontos.
+
+################# VERSÃO 1
+
+# import random
+
+# dicionario = {
+#     "machado": {
+#         1: "é um sobrenome",
+#         2: "corta madeira",
+#         3: "de assis"
+#     },
+#     "são paulo": {
+#         1: "nome de santo",
+#         2: "nome de cidade",
+#         3: "nome de time"
+#     },
+#     "planeta": {
+#         1: "o sol não é",
+#         2: "a lua não é",
+#         3: "a terra é"
+#     },
+#     "sanfona": {
+#         1: "instrumento musical",
+#         2: "fole",
+#         3: "luiz gonzaga"
+#     },
+#     "águia": {
+#         1: "ave com bico",
+#         2: "carnivora",
+#         3: "garras"
+#     },
+#     "alergia": {
+#         1: "poeira",
+#         2: "tinta",
+#         3: "doença"
+#     },
+#     "açúcar": {
+#         1: "cana",
+#         2: "beterraba",
+#         3: "cristais"
+#     },
+#     "áfrica": {
+#         1: "antigo continente",
+#         2: "deserto do saara",
+#         3: "leões e elefantes"
+#     },
+#     "adolescente": {
+#         1: "rebelde",
+#         2: "irresponsável",
+#         3: "quase adulto"
+#     },
+#     "aeroporto": {
+#         1: "pista",
+#         2: "hangar",
+#         3: "avião"
+#     },
+#     "agricultura": {
+#         1: "terra",
+#         2: "cultivo",
+#         3: "camponês"
+#     },
+#     "neblina": {
+#         1: "nevoeiro",
+#         2: "trevas",
+#         3: "nuvens"
+#     },
+#     "edifício": {
+#         1: "construção",
+#         2: "apartamentos",
+#         3: "arranha-céu"
+#     },
+#     "flauta": {
+#         1: "instrumento musical",
+#         2: "buracos",
+#         3: "sopro"
+#     }
+# }
+
+# palavra, dicas = random.choice(list(dicionario.items()))
+
+# print("Vamos começar nosso jogo, vocês receberão dicas para tentar acertar uma palavra!")
+# acertou = False
+# iterador = 1
+
+# while acertou == False and iterador <= 3:
+#     print(f"A primeira dica é: {dicas[iterador]}")
+#     chute1 = input("Jogador 1, tente acertar a palavra: ")
+#     if chute1 == palavra:
+#         print("Jogador 1, você venceu!")
+#         acertou = True
+#     else:
+#         print(f"\nJogador 2, agora é sua vez de tentar acertar a palavra. \nA primeira dica foi: {dicas[iterador]}.")
+#         iterador += 1 
+#         print(f"E a segunda dica é {dicas[iterador]}.")
+#         chute2 = input("Qual seu chute?: ")
+#         if chute2 == palavra:
+#             print("Jogador 2, você venceu!")
+#             acertou = True
+#         else:
+#             print(f"\nJogador 1, agora é sua vez de tentar acertar a palavra. \nA primeira dica foi {dicas[iterador - 1]}. \nA segunda dica foi: {dicas[iterador]}.")
+#             iterador += 1
+#             print(f"E a terceira dica é {dicas[iterador]}.")
+#             chute3 = input("Tente acertar a palavra, Jogador 1!: ")
+#             if chute3 == palavra:
+#                 print("Jogador 1, você venceu!")
+#                 acertou = True
+#             else:
+#                 print(f"\nJogador 2, agora é sua última vez de tentar acertar a palavra. \nA primeira dica foi {dicas[iterador - 1]}. \nA segunda dica foi: {dicas[iterador]}.")
+#                 print(f"E a terceira dica é {dicas[iterador]}.")
+#                 chute4 = input("Tente acertar a palavra, Jogador 2!: ")
+#                 if chute4 == palavra:
+#                     print("Jogador 2, você venceu!")
+#                     acertou = True
+#                 else:
+#                     iterador += 1
+#                     print("O jogo acabou e nenhum dos dois venceu... Mais sorte na próxima!")
+
+
+################# VERSÃO 2
+
 import random
 
 dicionario = {
     "machado": {
-        1: "é um sobrenome",
-        2: "corta madeira",
-        3: "de assis"
+        "dica_1": "é um sobrenome",
+        "dica_2": "corta madeira",
+        "dica_3": "de assis"
     },
     "são paulo": {
-        1: "nome de santo",
-        2: "nome de cidade",
-        3: "nome de time"
+        "dica_1": "nome de santo",
+        "dica_2": "nome de cidade",
+        "dica_3": "nome de time"
     },
     "planeta": {
-        1: "o sol não é",
-        2: "a lua não é",
-        3: "a terra é"
+        "dica_1": "o sol não é",
+        "dica_2": "a lua não é",
+        "dica_3": "a terra é"
     },
     "sanfona": {
-        1: "instrumento musical",
-        2: "fole",
-        3: "luiz gonzaga"
+        "dica_1": "instrumento musical",
+        "dica_2": "fole",
+        "dica_3": "luiz gonzaga"
     },
     "águia": {
-        1: "ave com bico",
-        2: "carnivora",
-        3: "garras"
+        "dica_1": "ave com bico",
+        "dica_2": "carnivora",
+        "dica_3": "garras"
     },
     "alergia": {
-        1: "poeira",
-        2: "tinta",
-        3: "doença"
+        "dica_1": "poeira",
+        "dica_2": "tinta",
+        "dica_3": "doença"
     },
     "açúcar": {
-        1: "cana",
-        2: "beterraba",
-        3: "cristais"
+        "dica_1": "cana",
+        "dica_2": "beterraba",
+        "dica_3": "cristais"
     },
     "áfrica": {
-        1: "antigo continente",
-        2: "deserto do saara",
-        3: "leões e elefantes"
+        "dica_1": "antigo continente",
+        "dica_2": "deserto do saara",
+        "dica_3": "leões e elefantes"
     },
     "adolescente": {
-        1: "rebelde",
-        2: "irresponsável",
-        3: "quase adulto"
+        "dica_1": "rebelde",
+        "dica_2": "irresponsável",
+        "dica_3": "quase adulto"
     },
     "aeroporto": {
-        1: "pista",
-        2: "hangar",
-        3: "avião"
+        "dica_1": "pista",
+        "dica_2": "hangar",
+        "dica_3": "avião"
     },
     "agricultura": {
-        1: "terra",
-        2: "cultivo",
-        3: "camponês"
+        "dica_1": "terra",
+        "dica_2": "cultivo",
+        "dica_3": "camponês"
     },
     "neblina": {
-        1: "nevoeiro",
-        2: "trevas",
-        3: "nuvens"
+        "dica_1": "nevoeiro",
+        "dica_2": "trevas",
+        "dica_3": "nuvens"
     },
     "edifício": {
-        1: "construção",
-        2: "apartamentos",
-        3: "arranha-céu"
+        "dica_1": "construção",
+        "dica_2": "apartamentos",
+        "dica_3": "arranha-céu"
     },
     "flauta": {
-        1: "instrumento musical",
-        2: "buracos",
-        3: "sopro"
+        "dica_1": "instrumento musical",
+        "dica_2": "buracos",
+        "dica_3": "sopro"
     }
 }
 
@@ -81,18 +202,21 @@ palavra, dicas = random.choice(list(dicionario.items()))
 
 print("Vamos começar nosso jogo, vocês receberão dicas para tentar acertar uma palavra!")
 acertou = False
-iterador = 1
+round = 1
 
-while acertou == False and iterador <= 3:
-    print(f"A primeira dica é: {dicas[iterador]}")
-    chute1 = input("Jogador 1, tente acertar a palavra: ")
+##### FALTAM AJUSTES
+
+while acertou == False and round <= 10:
+    jogador = 1
+    print(f"A primeira dica é: {dicas["dica_1"]}")
+    chute1 = input("Jogador {}, tente acertar a palavra: ")
     if chute1 == palavra:
         print("Jogador 1, você venceu!")
         acertou = True
     else:
-        print(f"\nJogador 2, agora é sua vez de tentar acertar a palavra. \nA primeira dica foi: {dicas[iterador]}.")
+        print(f"\nJogador {}, agora é sua vez de tentar acertar a palavra. \nA primeira dica foi: {dicas["dica_1"]}.")
         iterador += 1 
-        print(f"E a segunda dica é {dicas[iterador]}.")
+        print(f"E a segunda dica é {dicas["dica_02"]}.")
         chute2 = input("Qual seu chute?: ")
         if chute2 == palavra:
             print("Jogador 2, você venceu!")
@@ -115,7 +239,5 @@ while acertou == False and iterador <= 3:
                 else:
                     iterador += 1
                     print("O jogo acabou e nenhum dos dois venceu... Mais sorte na próxima!")
-
-
 
 
