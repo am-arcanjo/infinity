6985ac5a417d757302943197686af34acd3e565b
PauloAhouagi <pauloahouagi@gmail.com>
2024-10-16 19:43:23 -0300

    renomeamento, anotando aula de poo

---
+++ Python/AULAS_11_EM_DIANTE/poo_parte_2.py
@@ -0,0 +1,26 @@
+#A aula 11 eu não compareci, mas foi de POO, foi dado um basicão sobre o que é, o que significa na prática, etc
+
+#Exercício: criar classe pessoa que tem atributos nome e idade, e um método apresentar que imprime esses atributos. Crie uma subclasse que herda de pessoa chamada Estudante, que tem além de nome e idade da classe superlativa, um atributo curso. Apresentar aqui deve também imprimir o curso. E crie mais uma subclasse chamada Professor, que também herda de Pessoa. Crie um método apresentar com uma string adequada.
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+class Estudante(Pessoa):
+    def __init__(self, nome, idade, turma):
+        super.__init__(nome, idade)
+        self.turma = turma
+    
+    def apresentar(nome, idade, turma):
+        print("Olá, eu sou o aluno {self.nome}, tenho {self.idade} anos e estou na turma {self.turma}.")
+
+class Professor(Pessoa):
+    def __init__(self, nome, idade):
+        super.__init__(nome, idade)
+    
+    def apresentar(nome, idade):
+        print("Olá, eu sou o professor {nome}, e tenho {idade} anos.")
+        
+Estudante.apresentar("Rico", 34, "Biologia molecular")
