e39821028a8edfc18e69e354de99fb2ecd5fbc4f
PauloAhouagi <pauloahouagi@gmail.com>
2024-10-16 20:26:41 -0300

    ajustes e exercício 3

--- Python/AULAS_11_EM_DIANTE/poo_parte_2.py
+++ Python/AULAS_11_EM_DIANTE/poo_parte_2.py
@@ -4,29 +4,29 @@
 
 import random
 
-class Pessoa:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
+# class Pessoa:
+#     def __init__(self, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
 
 
-class Estudante(Pessoa):
-    def __init__(self, nome, idade, turma):
-        super.__init__(nome, idade)
-        self.turma = turma
+# class Estudante(Pessoa):
+#     def __init__(self, nome, idade, turma):
+#         super.__init__(nome, idade)
+#         self.turma = turma
     
-    def apresentar(nome, idade, turma):
-        print(f"Olá, eu sou o aluno {nome}, tenho {idade} anos e estou na turma {turma}.")
+#     def apresentar(nome, idade, turma):
+#         print(f"Olá, eu sou o aluno {nome}, tenho {idade} anos e estou na turma {turma}.")
 
-class Professor(Pessoa):
-    def __init__(self, nome, idade):
-        super.__init__(nome, idade)
+# class Professor(Pessoa):
+#     def __init__(self, nome, idade):
+#         super.__init__(nome, idade)
     
-    def apresentar(nome, idade):
-        print(f"Olá, eu sou o professor {nome}, e tenho {idade} anos.")
+#     def apresentar(nome, idade):
+#         print(f"Olá, eu sou o professor {nome}, e tenho {idade} anos.")
         
-Estudante.apresentar("Rico", 34, "Biologia Molecular")
-Professor.apresentar("Gaspar", 51)
+# Estudante.apresentar("Rico", 34, "Biologia Molecular")
+# Professor.apresentar("Gaspar", 51)
 
 #Exercício 2: Crie uma classe chamada Animal que tenha um atributo nome. Adicione um método de fazer som que retorne o som do animal. Depois crie uma classe cachorro (ou outra) que sobrescreva o método de fazer som para retornar a onomatópeia deste animal. Crie um objeto do novo animal com o nome dele e imprima o som que ele faz.
 
@@ -49,3 +49,35 @@ class Raposa:
         print(f"Esta é a raposa {nome}, e what does the fox say? {fox_sound}")
 
 Raposa.fazer_som("Jackie")
+
+
+#Exercício 3: Crie uma classe funcionário com atributos nome e salário. Em seguida, crie duas classes que herdem de Funcionário: Geremte (que além desses dois anteriores tem um atributo bônus) e Atendente (que além dos citados tem também o atributo turno). Além disso, pra classe Gerente crie um método que informa somente o bônus do gerente.
+
+class Funcionario:
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+    
+
+class Gerente(Funcionario):
+    def __init__(self, nome, salario, bonus):
+        super.__init__(nome, salario)
+        self.bonus = bonus
+
+    def imprimir_gerente(nome, salario, bonus):
+        print(f"O nome deste gerente é {nome}, seu salário é de {salario} reais e o seu bônus de performance é {bonus}.")
+    
+    def imprimir_bonus(bonus):
+        print(f"O seu bônus é: {bonus}.")
+
+class Atendente(Funcionario):
+    def __init__(self, nome, salario, turno):
+        super.__init__(nome, salario)
+        self.turno = turno
+    
+    def imprimir_atendente(nome, salario, turno):
+        print(f"O nome deste atendente é {nome}, seu salário é de {salario} reais e seu turno é o da {turno}.")
+
+Atendente.imprimir_atendente("Marciano", 1800, "noite")
+Gerente.imprimir_gerente("Rachel", 2400, 500)
+        
