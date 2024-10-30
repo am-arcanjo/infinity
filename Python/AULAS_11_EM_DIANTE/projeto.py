f497e10cc2b4ab7e6b60453f9cf45cf41e3b73bd
TrivelatoRob <robertotjr@gmail.com>
2024-10-30 19:42:01 -0300

    mais do projeto

--- Python/AULAS_11_EM_DIANTE/projeto.py
+++ Python/AULAS_11_EM_DIANTE/projeto.py
@@ -22,9 +22,23 @@ class Quarto:
         self.tipo = tipo
         self.preco = preco
         self.disponibilidade = True
+    
         
-#class Reserva(Cliente, Quarto):
-#    def __init__():
+class Reserva:
+    def __init__(self, cliente: Cliente, quarto: Quarto, data):
+        self.cliente = cliente
+        self.quarto = quarto
+        self.data = data
+        self.confirmada = False
+    
+    def confirmar_reserva(self):
+        self.confirmada = True
+        self.quarto.disponibilidade = False
+    
+    
+class GerenciadorDeReservas:
+    def __init__(self, reserva: Reserva):
+            self.reserva = reserva    
         
 
 quarto_1 = Quarto(101, "Básico" , 150)
